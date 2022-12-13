abc0c3172451ddcab1577da6676881d14d690e0d
toobrian <115666998+toobrian@users.noreply.github.com>
2022-12-13 13:44:36 -0800

    this work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,17 @@
-#!/bin/bash
-puts ("\"Programming is like building a multilingual puzzle");
+#include <stdio.h>
+
+
+/**
+ *main - prints "Programming is like building a multilingual
+ * puzzle, followed by a newline."
+ * return 0
+ */
+
+int main (void)
+
+{
+	puts("\"Programming is like buiilding a multilingual puzzle");
+
+	return (0);
+
+}
