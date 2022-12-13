47cbe7713a49ad9970feb99af770b716ba7fdf1b
toobrian <115666998+toobrian@users.noreply.github.com>
2022-12-13 15:18:20 -0800

    hey

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types based 
- * on the computer it is compiled and run on.
- * Return: Always 0.
- */
+*main - Prints the size of various types based on
+*the computer it is compiled and run on.
+*Return: Always 0.
+*/
 
 int main(void)
 
