ecf049b80249e3df16572e83f37ec29c0efa259a
toobrian <115666998+toobrian@users.noreply.github.com>
2023-06-15 13:37:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A C program that prints a line to the standard error
- * Retrun 1 (Success)
+ * return 1 (Success)
  */
-int main()
+int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar,2015-10-19\n", stdout);
-	return (1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1) /* this is the return */
 }
