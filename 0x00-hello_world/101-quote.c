1003f8619c43f4c448e8c4a3bbadedcc48ff903f
toobrian <115666998+toobrian@users.noreply.github.com>
2023-06-15 13:30:21 -0700

    1010 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main - A C program that prints a line to the standard error
  * Retrun 1 (Success)
  */
-int main(void)
+int main()
 {
-	fputs("and that piece of art is useful\" - Dora Korpar.2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar,2015-10-19\n", stdout);
 	return (1);
 }
