5612ed7226b19214a0533c765bf2e7a4b480eb78
toobrian <115666998+toobrian@users.noreply.github.com>
2023-06-20 17:03:48 -0700

    ./3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks for lowercase characters
- * @c - the character to be checked
+ * @c: - the character to be checked
  * Return: 1 for lowercase charcater or 0 for anythign else
  */
 
